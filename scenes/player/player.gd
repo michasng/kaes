55081ab557f6acea2be64e577730957c99700018
Micha Sengotta <micha.sengotta@gmail.com>
2025-06-07 14:04:54 +0200

    Add basic platformer setup

---
+++ scenes/player/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -200.0
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if velocity.x == 0:
+		animated_sprite.stop()
+	else:
+		animated_sprite.flip_h = velocity.x < 0
+		animated_sprite.play("walk")
+
+	move_and_slide()
