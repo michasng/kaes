4ad24f561a65f0f8b6788ecc4242cb8aa6f10700
Micha Sengotta <micha.sengotta@gmail.com>
2025-06-07 19:12:27 +0200

    Add player hat

--- scenes/player/player_sprite.gd
+++ scenes/player/player_sprite.gd
@@ -1,15 +1,29 @@
-class_name PlayerSprite
-extends AnimatedSprite2D
+class_name PlayerSprite extends AnimatedSprite2D
 
-@export var body: CharacterBody2D
+var character: CharacterBody2D
+@onready var hat_sprite: Sprite2D = $HatSprite2D
+
+const _ANIMATION_BY_STATE_NAME: Dictionary = {
+	"IdleMovementState": "idle",
+	"WalkMovementState": "walk",
+	"JumpMovementState": "jump",
+	"FallMovementState": "fall",
+}
+
+func _ready() -> void:
+	await owner.ready
+	character = owner as Character
+	assert(character != null)
 
 func _process(_delta: float) -> void:
-	if body.velocity.x != 0:
-		flip_h = body.velocity.x < 0
+	if character.velocity.x != 0:
+		flip_h = character.velocity.x < 0
+		hat_sprite.flip_h = flip_h
 
 func _on_movement_state_changed(_previous_state: State, current_state: State) -> void:
-	match current_state.name:
-		"IdleMovementState": play("idle")
-		"WalkMovementState": play("walk")
-		"JumpMovementState": play("jump")
-		"FallMovementState": play("fall")
+	play(_ANIMATION_BY_STATE_NAME[current_state.name])
+
+func update_hat_position() -> void:
+	# some frames of some animations have a lower head, so the hat must follow
+	var hat_needs_offset = animation == "fall" or (animation == "walk" and frame % 2 == 0)
+	hat_sprite.offset = Vector2(0, 1 if hat_needs_offset else 0)
