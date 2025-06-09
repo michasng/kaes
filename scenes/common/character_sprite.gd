21089c71b0d93716079bfb5a30aabdab455a2986
Micha Sengotta <micha.sengotta@gmail.com>
2025-06-09 09:47:46 +0200

    Improve player sprite readability

--- scenes/common/character_sprite.gd
+++ scenes/common/character_sprite.gd
@@ -2,8 +2,7 @@ class_name CharacterSprite extends AnimatedSprite2D
 
 var character: CharacterBody2D
 
-@onready var on_head: Node2D = $OnHead
-@onready var in_hand: Node2D = $InHand
+@onready var children: Array[Node] = self.find_children("*", "Node2D", false)
 
 const _ANIMATION_BY_STATE_NAME: Dictionary = {
 	"IdleMovementState": "idle",
@@ -29,10 +28,8 @@ func _on_movement_state_changed(_previous_state: State, current_state: State) ->
 	play(_ANIMATION_BY_STATE_NAME[current_state.name])
 
 
-# some frames of some animations have a lower head or hand, so the dependent sprites must follow
+# some frames of some animations have a lower head and hand, so dependent sprites must follow
 func update_offsets() -> void:
-	var head_offset_needed = animation == "fall" or (animation == "walk" and frame % 2 == 0)
-	on_head.position = Vector2(0, 1 if head_offset_needed else 0)
-
-	var hand_offset_needed = animation == "fall" or animation == "jump" or (animation == "walk" and frame % 2 == 0)
-	in_hand.position = Vector2(0, 1 if hand_offset_needed else 0)
+	var offset_needed = animation == "fall" or (animation == "walk" and frame % 2 == 0)
+	for child: Node2D in children:
+		child.position = Vector2(0, 1 if offset_needed else 0)
